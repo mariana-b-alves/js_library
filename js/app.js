652ecb13dd0e4f282d0be3303bf6fd7a49c42ea2
Mariana Alves <m.bourdain.alves>
2025-04-07 15:19:47 +0100

    JS library uploaded to GH

---
+++ js/app.js
@@ -0,0 +1,71 @@
+//Array with information about the following books:
+
+let books = [
+            {
+                title: 'Angular Com Typescript',
+                author: "Yakov Fain",
+                alreadyRead: true,
+                imageUrl: 'js_library/livros/angular.jpg',
+            },
+            {
+                title: 'Blockchain com JS',
+                author: "Bina Ramamurthy",
+                alreadyRead: false,
+                imageUrl: 'exercicioDOM-2-inicio/livros/blockchain.jpg',
+            },
+            {
+                title: 'Deep Learning com JS',
+                author: "Various Authors",
+                alreadyRead: true,
+                imageUrl: 'js_library/livros/deeplearning.jpg',
+            },
+            {
+                title: 'Joy Of Javascript',
+                author: "Luis Ascencio",
+                alreadyRead: false,
+                imageUrl: 'js_library/livros/joj.jpg',
+            },
+            {
+                title: 'React Hooks in Action',
+                author: "John Larsen",
+                alreadyRead: true,
+                imageUrl: 'js_library/livros/reacthooks.jpg', //Original code changed as I have tonnes and tonnes of subfolders
+                                                                                    //Otherwise, the links would be broken
+                                                                                    //And there would be no pics for anyone
+            },
+        ];
+
+//Select section from the original HTML document
+let section = document.querySelector("section");
+
+//If i is more than 0, under the length of the array, and increments..
+for (let i = 0; i < books.length; i++) {  
+    //...Create an article inside 
+    let article = document.createElement("article");
+    //...Create a variable (bookArray) containing everything inside the books variable
+    let bookArray = books[i];
+
+
+  //Gather info from bookArray and add it to the original HTML
+  //Rather than writing everything by hand, by adding ${variable.nestedArrayItem},
+  //the information is put in its respective places with minimal coding
+  //Also, I didn't add classes. No need, imo. :D
+  article.innerHTML += `
+    <h1>${bookArray.title}</h1>
+    <img src='${bookArray.imageUrl}' alt='${bookArray.title}'>
+    <p>${bookArray.author}</p>
+    `;
+
+    //If the book was already read, then...
+    if (bookArray.alreadyRead == true) {
+        //Add the following text directly to the original HTML
+        article.innerHTML += `<p>Already Read: ✔️</p>`
+    } else { //If not / is false, then...
+        //Add the following text directly to the original HTML
+        article.innerHTML += `<p>Already Read: ✖️</p>`
+    }
+    //Add all the content from the article variable to our section (and, therefore, to the HTML)
+    section.append(article);
+ 
+} //Code done!
+
